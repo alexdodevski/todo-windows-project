b00e909c033aade5298ffedae8c19a354b73a648
Alexander Dodevski <alexanderdodevski@gmail.com>
2022-07-24 01:53:52 +0300

    add scrollbar for css-layout

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -9,7 +9,11 @@ export class Main extends TodoComponent {
     });
   }
   toHTML() {
-    return ` <div class="todo__main__task">
+    return ` 
+          <div class="todo__main__viewport">
+          <div class="todo__main__content_box">
+          <div class="todo__main__content_tasks">
+            <div class="todo__main__task">
             <div class="task__done">
               <span class="material-symbols-outlined btn_done"> done </span>
             </div>
@@ -22,6 +26,11 @@ export class Main extends TodoComponent {
             <div class="task__btn_favorite">
               <span class="material-symbols-rounded btn_favorite"> star </span>
             </div>
-          </div>`;
+          </div>
+</div>
+          </div>
+          </div>
+
+ `;
   }
 }
